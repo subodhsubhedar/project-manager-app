a41a9f498e8fe682b742186b3763eb31deaccfcd
subodhsubhedar <42935075+subodhsubhedar@users.noreply.github.com>
2019-06-23 13:57:11 +0530

    Initial version

---
+++ project-manager-app/src/main/java/com/myapp/ProjectManagerAppApplication.java
@@ -0,0 +1,13 @@
+package com.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerAppApplication.class, args);
+	}
+
+}
